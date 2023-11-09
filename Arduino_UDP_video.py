f90a5c98bb85d4f77595b62eb4c86ea3f0122a36
hajo1038 <jpl.haller@gmail.com>
2023-11-09 17:17:32 +0100

    add LED to UDP video and improvements

--- Arduino_UDP_video.py
+++ Arduino_UDP_video.py
@@ -20,11 +20,14 @@ WIDTH=400
 
 BUFF_SIZE = 65536
 
+LED3 = Pin("PE12", Pin.OUT_PP)
+LED3.value(0)
+
 def connect_to_AP():
     # Init wlan module and connect to network
     print("Trying to connect. Note this may take a while...")
     wlan = network.WLAN(network.STA_IF)
-    wlan.ifconfig(('192.168.1.2', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
+    #wlan.ifconfig(('192.168.1.2', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
     wlan.deinit()
     wlan.active(True)
     connection_counter = 1
@@ -52,6 +55,7 @@ socket_address = (HOST_IP, port) # own socket adress
 #server_socket.bind(socket_address)
 #print('Listening at:',socket_address)
 
+
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QQVGA)   # Set frame size to QVGA (320x240)
@@ -62,6 +66,7 @@ img = sensor.snapshot()
 part_counter = 1
 motor = Motor(PWM_frequency=2000)
 motor_percentage = 40
+LED3.value(1)
 while True:
     #start = pyb.micros()
     if motor_percentage < 80:
@@ -69,19 +74,24 @@ while True:
     #motor.drive_forward(motor_percentage)
     img = sensor.snapshot()
     #base64_data = binascii.b2a_base64(img)
-    #img.compress()
+    #start = pyb.micros()
+    img.compress()
+    #print(pyb.micros() - start)
     test_data = binascii.b2a_base64(img)
-    #print(test_data)
     #print(len(binascii.a2b_base64(test_data)))
     img_data = bytearray(binascii.a2b_base64(test_data))
-
-    for part_counter in range(1,5):
-            lower_index = int((len(img_data)/4)*(part_counter-1))
-            upper_index = int((len(img_data)/4)*(part_counter))
-            packet = img_data[lower_index:upper_index]
-            packet.extend(part_counter.to_bytes(1,'big'))
+    try:
+        server_socket.sendto(img_data,("192.168.1.2",4210))
+    except:
+        LED3.value(0)
+    #print(img_data)
+    # for part_counter in range(1,5):
+    #         lower_index = int((len(img_data)/4)*(part_counter-1))
+    #         upper_index = int((len(img_data)/4)*(part_counter))
+    #         packet = img_data[lower_index:upper_index]
+    #         packet.extend(part_counter.to_bytes(1,'big'))
             #print(f"{lower_index} : {upper_index}")
-            server_socket.sendto(packet,("192.168.1.3",4210))
+            # server_socket.sendto(packet,("192.168.1.2",4210))
     #msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
     #print('GOT connection from ',client_addr)
     #print(pyb.micros() - start)
