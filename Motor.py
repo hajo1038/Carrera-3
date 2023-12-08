b8b996ecadaf3cd551f7807f87e7aa4f48a9b7e2
hajo1038 <jpl.haller@gmail.com>
2023-12-08 11:21:36 +0100

    update motor class

--- Motor.py
+++ Motor.py
@@ -12,17 +12,17 @@ from ina219 import INA219
 class Motor:
     def __init__(self, PWM_frequency):
         #Pin assignments
-        AIN1 = Pin("PE11", Pin.OUT_PP)
-        AIN2 = Pin("PF3", Pin.OUT_PP)
+        self.AIN1 = Pin("PE11", Pin.OUT_PP)
+        self.AIN2 = Pin("PF3", Pin.OUT_PP)
         PWM_pin = Pin("PE13")
         #STBY = Pin(6, Pin.OUT_PP)
 
-        tim = Timer(1, freq=PWM_frequency)
-        pwm_channel = tim.channel(3, Timer.PWM, pin=PWM_pin)
-        AIN1.value(0)
-        AIN2.value(0)
+        self.PWM_tim = Timer(1, freq=PWM_frequency)
+        self.pwm_channel = self.PWM_tim.channel(3, Timer.PWM, pin=PWM_pin)
+        self.AIN1.value(0)
+        self.AIN2.value(0)
 
     def drive_forward(self, percentage):
-        AIN1.value(1)
-        AIN2.value(0)
-        pwm_channel.pulse_width_percent(percentage)
+        self.AIN1.value(1)
+        self.AIN2.value(0)
+        self.pwm_channel.pulse_width_percent(percentage)
