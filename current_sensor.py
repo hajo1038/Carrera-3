e7f74aad8ef7efb44fe1a8313f6ba9a7f31f0c5b
hajo1038 <jpl.haller@gmail.com>
2023-12-08 11:20:14 +0100

    update current measurement

--- current_sensor.py
+++ current_sensor.py
@@ -6,11 +6,12 @@ class CurrentSensor:
     def __init__(self):
         self.I2C_INTERFACE_NO = 1 #select I2C Interface
         self.SHUNT_OHMS = 0.1 #value of shunt resistor in ohm
-        self.ina = INA219(self.SHUNT_OHMS, I2C(self.I2C_INTERFACE_NO)) #creates instance of class
+        i2c_test = I2C(self.I2C_INTERFACE_NO)
+        self.ina = INA219(self.SHUNT_OHMS, i2c_test) #creates instance of class
         self.ina.configure(self.ina.RANGE_16V, self.ina.GAIN_8_320MV)
 
          # Moving average for current measurement
-        self.current_windows_size = 4
+        self.current_windows_size = 8
         self.current_avg_sum = 0
         self.current_readings = [0] * self.current_windows_size
         self.current_reading_index = 0
@@ -19,7 +20,7 @@ class CurrentSensor:
 
 
     def get_actual_current(self):
-        actual_current = self.ina.current() * (-1.0)
+        actual_current = int(self.ina.current() * (-1.0))
         return actual_current
 
 
